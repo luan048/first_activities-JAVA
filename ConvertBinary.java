0794732953cc0068199371b9731990a23381955b
luan048 <luanmonteiropedrosa@gmail.com>
2024-10-23 11:40:00 -0300

    Convert Binary

---
+++ ConvertBinary.java
@@ -0,0 +1,10 @@
+public class ConvertBinary {
+
+    public static int toBinary(int n) {
+        return Integer.valueOf(Integer.toBinaryString(n));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toBinary(10));
+    }
+}
